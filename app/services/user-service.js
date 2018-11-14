9a4f754509e10ad67261beacc11679b61b97768b
daliannyvieira <vieiradalianny@gmail.com>
2018-11-14 19:38:36 -0200

    fix user service

--- app/services/user-service.js
+++ app/services/user-service.js
@@ -17,67 +17,88 @@ module.exports = class Users {
 
   findUsersList() {
     this.router.get('/users', async (req, res) => {
-      await User.findAll().map(user => Json.formatUser(user))
-        .then(users => res.status(200).json(users))
-        .catch(err => res.status(500).json(err))
+      try {
+        const users = await User.findAll().map(user => Json.formatUser(user))
+        res.status(200).json(users)
+      }
+      catch (err) {
+        res.status(500).json(err)
+      }
     });
   }
 
   createUser() {
     this.router.post('/users', async (req, res) => {
-      await User.create(req.body)
-        .then(user => res.status(200).json(Json.formatUser(user)))
-        .catch(err => res.status(500).json(err))
+      try {
+        const user = await User.create(req.body)
+        res.status(200).json(Json.formatUser(user))
+      }
+      catch (err) {
+        res.status(500).json(err)
+      }
     });
   }
 
   findUser() {
     this.router.get('/users/:id', async (req, res) => {
-      await User.find({ where: { id: req.params.id } })
-        .then(user => {
-          if (!user) {
-            res.status(404).json({ message: 'Didn’t find anything here!' });
-          }
+      try {
+        const user = await User.find({ where: { id: req.params.id } })
+        if (user) {
           res.status(200).json(Json.formatUser(user));
-        })
-        .catch(err => res.status(500).json(err))
+        }
+        else {
+          res.status(404).json({ message: 'Didn’t find anything here!' });
+        }
+      }
+      catch (err) {
+        res.status(500).json(err)
+      }
     });
   }
 
   updateUser() {
     this.router.put('/users/:id', async (req, res) => {
-      const id = req.params.id;
-      const updates = req.body;
-      
-      const user = await User.find({ where: { id: id } })
+      try {
+        const id = req.params.id;
+        const updates = req.body;
+        
+        const user = await User.find({ where: { id: id } })
 
-      if (user) {
-        User.update(updates, { where: { id: id } })
-        res.status(200).json({
-          type: 'User',
-          message: 'User ' + id + ' was updated with success!'
-        });
+        if (user) {
+          User.update(updates, { where: { id: id } })
+          res.status(200).json({
+            type: 'User',
+            message: 'User ' + id + ' was updated with success!'
+          });
+        }
+
+        else {
+          res.status(404).json({ message: 'Didn’t find anything here!' });
+        }
       }
-
-      else {
-        res.status(404).json({ message: 'Didn’t find anything here!' });
+      catch (err){
+        res.status(500).json(err);
       }
     });
   }
 
   deleteUser() {
     this.router.delete('/users/:id', async (req, res) => {
-      const id = req.params.id;
+      try {
+        const id = req.params.id;
 
-      let deleted = await User.destroy({ where: { id: id } })
+        let deleted = await User.destroy({ where: { id: id } })
 
-      if (deleted) {
-        res.json({type: 'User', message: 'User ' + id + ' was deleted with success!' });
+        if (deleted) {
+          res.json({type: 'User', message: 'User ' + id + ' was deleted with success!' });
+        }
+        else {
+          res.status(404).json({ message: 'Didn’t find anything here!' });
+        }
       }
-      else {
-        res.status(404).json({ message: 'Didn’t find anything here!' });
+      catch (err){
+        res.status(500).json(err);
       }
-
     });
   }
 
