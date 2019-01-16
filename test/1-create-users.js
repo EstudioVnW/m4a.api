da3c59377fb87a2cd1fbcfd704ebf18c9edc255c
daliannyvieira <vieiradalianny@gmail.com>
2019-01-16 18:59:04 -0200

    clean code

--- test/1-create-users.js
+++ test/1-create-users.js
@@ -1,8 +1,8 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-var server = require('../index');
-var should = chai.should();
+const server = require('../index');
+const should = chai.should();
 const expect = chai.expect;
 
 describe('Create a new user', () => {
