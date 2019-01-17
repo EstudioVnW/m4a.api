685924909dd685e8bafc9de0267a91351e4602cb
daliannyvieira <vieiradalianny@gmail.com>
2019-01-17 15:49:03 -0200

    adding upload avatar

--- app/services/user-service.js
+++ app/services/user-service.js
@@ -1,6 +1,49 @@
 'use strict';
 const { User, Match, Initiative, UsersInterests } = require('../../domain/entities');
 const Json = require('../responses/users');
+const multer = require('multer');
+const firebase = require('firebase');
+const {Storage} = require('@google-cloud/storage');
+const fs = require('fs')
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../config/config.js')[env];
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10000
+  }
+});
+
+const cleanFiles = (file) => {
+  fs.unlink(file, (err) => {
+    if (err) throw err;
+    console.log('clean successfully:)');
+  });
+}
+
+const createPublicFileURL = (storageName) => `http://storage.googleapis.com/${bucketName}/${encodeURIComponent(storageName)}`;
+
+const projectId = config.projectId
+const bucketName = `${projectId}.appspot.com`;
+const keyFilename='./config/firebase-adminsdk.json';
+
+// Creates a client
+const firebaseStorage = new Storage({
+  projectId: projectId,
+  keyFilename: keyFilename
+});
+
+const bucket = firebaseStorage.bucket(bucketName);
 
 module.exports = class Users {
   constructor(router) {
@@ -13,6 +56,30 @@ module.exports = class Users {
     this.findUser();
     this.updateUser();
     this.deleteUser();
+    this.uploadAvatar();
+  }
+
+  uploadAvatar() {
+    this.router.post("/users/uploadAvatar", upload.single('avatar'), async (req, res) => {
+      try {
+        bucket.upload(req.file.path, {
+          destination: `user-avatar/${req.file.filename}`,
+          public: true,
+        }, (err, file) => {
+          if (err) {
+            console.log(err);
+            cleanFiles(req.file.path)
+            return;
+          }
+          cleanFiles(req.file.path)
+          res.status(200).json({message: createPublicFileURL(req.file.path)})
+        });
+      }
+      catch (err) {
+        cleanFiles(req.file.path)
+        res.status(500).json({message: 'something is broken', err: err})
+      }
+    });
   }
 
   findUsersList() {
--- config/config.js
+++ config/config.js
@@ -7,20 +7,23 @@ module.exports = {
     "password": process.env.RELACIONAL_DB_PASS,
     "database": process.env.RELACIONAL_DB_NAME,
     "host": process.env.RELACIONAL_DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "projectId": process.env.PROJECT_ID
   },
   "test": {
     "username": process.env.TEST_RELACIONAL_DB_USERNAME,
     "password": process.env.TEST_RELACIONAL_DB_PASS,
     "database": process.env.TEST_RELACIONAL_DB_NAME,
     "host": process.env.TEST_RELACIONAL_DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "projectId": process.env.PROJECT_ID
   },
   "production": {
     "username": process.env.RELACIONAL_DB_USERNAME,
     "password": process.env.RELACIONAL_DB_PASS,
     "database": process.env.RELACIONAL_DB_NAME,
     "host": process.env.RELACIONAL_DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "projectId": process.env.PROJECT_ID
   }
 }
