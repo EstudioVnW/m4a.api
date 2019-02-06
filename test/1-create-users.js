13925b2463bc7e510b21361a0d5d1f778bb2849a
daliannyvieira <vieiradalianny@gmail.com>
2019-02-06 00:13:20 -0200

    update create user test

--- test/1-create-users.js
+++ test/1-create-users.js
@@ -1,63 +1,56 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+process.env.NODE_ENV = 'test';
+
+let { User } = require('../domain/entities');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
+
 chai.use(chaiHttp);
-const server = require('../index');
-const should = chai.should();
-const expect = chai.expect;
 
-describe('Create a new user', () => {
-
-  let user = {
-      "username": "dalinha2",
-      "email": "dali3@gmial.com",
-      "latlong": {
-        "type": "Point",
-        "coordinates": [
-            39.807222,
-            -76.984722
-        ]
-      },
-      "userProfile": "Volunteer"
-  }
-
-  it('should create an user', (done) => {
-    chai.request(server.app)
-      .post('/users')
-      .set('content-type', 'application/json')
-      .send(user)
-      .end((err, res, body) => {
-        if (err) {
-          done(err);
-        }
-        else {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.have.property('data');
-          done();
-        }
-      });
-
-  });
-
-  it('should return an auth to current user', (done) => {
-    chai.request(server.app)
-      .post('/login')
-      .set('content-type', 'application/json')
-      .send({
-        "email": user.email
+describe('Users', () => {
+/*  beforeEach((done) => {
+      User.destroy({ where: {}, truncate: true }, (err) => {
+        done();
       })
-      .end((err, res, body) => {
-        if (err) {
-          done(err);
-        }
-        else {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.have.property('data');
+  });
+*/
+  describe('/GET user', () => {
+    it('it should GET all the users', (done) => {
+      chai.request(server.app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
           done();
-        }
-      });
-
+        });
+    });
   });
 
+  describe('/POST user', () => {
+    it('it should not POST a user without pages field', (done) => {
+      let user = {
+        "email": "dalivieiraa8@gmail.com",
+        "username": "dali",
+        "userProfile": "Volunteer",
+        "interests": [58, 59, 60]
+      }
+      chai.request(server.app)
+        .post('/users')
+        .send(user)
+        .end((err, res) => {
+          console.log(res.body)
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('data');
+          res.body.should.have.property('token');
+          res.body.should.have.property('relationships');
+          res.body.data.should.have.property('type').eql('User');
+          res.body.data.should.have.property('attributes');
+          res.body.relationships.should.have.property('interests');
+          res.body.relationships.interests.should.be.a('array');
+          done();
+        });
+    });
+  });
 });
